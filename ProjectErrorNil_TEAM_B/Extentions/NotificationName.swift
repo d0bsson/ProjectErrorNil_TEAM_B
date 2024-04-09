1cf0225e7db36c0715271b33fe17b0cef4825412
David <ber.dev@yandex.ru>
2024-04-09 23:04:43 +0300

    Добавил static переменнную для вызова UserDefaults Работает переход в приложение после регистрации Приложение узнает уже залогиненных пользователей

---
+++ ProjectErrorNil_TEAM_B/Extentions/NotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName.swift
+//  ProjectErrorNil_TEAM_B
+//
+//  Created by d0bsson on 09.04.2024.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let loginNotification = Notification.Name("loginNotification")
+}
